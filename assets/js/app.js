e5fb2725a584374fbfbd5b62a69cbcec7e378fb4
Renan-Souzz <74443515+Renan-Souzz@users.noreply.github.com>
2024-07-01 18:22:32 -0300

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,111 @@
+var btn = document.querySelector(".no");
+var position = 0;
+var isAnimating = false;
+
+var btn = document.querySelector(".no");
+var position
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    btn.addEventListener("touchstart", function() {
+        position ? (position = 0) : (position = 150);
+        btn.style.transform = `translate(${position}px,0px)`;
+        btn.style.transition = "all 0.3s ease";
+      });
+
+      btn.addEventListener("touchend", function() {
+        position ? (position = 0) : (position = 150);
+        btn.style.transform = `translate(${position}px,0px)`;
+        btn.style.transition = "all 0.3s ease";
+      });
+  } else {
+    btn.addEventListener("mouseover", function() {
+        position ? (position = 0) : (position = 150);
+        btn.style.transform = `translate(${position}px,0px)`;
+        btn.style.transition = "all 0.3s ease";
+        });
+  }
+
+
+
+btn.addEventListener("click", function() {
+    if (!isAnimating) {
+        isAnimating = true;
+        position = position === 0 ? 150 : 0;
+        btn.style.transform = `translate(0px, ${position}px)`;
+        btn.style.transition = "all 0.2s ease";
+
+        setTimeout(function() {
+            isAnimating = false;
+        }, 200);
+    }
+});
+
+btn.addEventListener("mouseover", function() {
+    if (!isAnimating) {
+        isAnimating = true;
+        position = position === 0 ? 150 : 0;
+        btn.style.transform = `translate(0px, ${position}px)`;
+        btn.style.transition = "all 0.2s ease";
+
+        setTimeout(function() {
+            isAnimating = false;
+        }, 200);
+    }
+});
+
+const sim = document.getElementById('yes');
+
+sim.addEventListener("click", () => {
+
+let timerInterval
+Swal.fire({
+  title: 'Obrigado 😍',
+  html: 'Vai ter chopp de Vinho. 💘',
+  timer: 2000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading()
+    const b = Swal.getHtmlContainer().querySelector('b')
+    timerInterval = setInterval(() => {
+      b.textContent = Swal.getTimerLeft()
+    }, 100)
+  },
+  willClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
+
+});
+
+const no = document.getElementById('no');
+
+no.addEventListener("click", () => {
+
+let timerInterval
+Swal.fire({
+  title: 'Qual Foi? 😒',
+  html: 'Volta lá e aperta no SIM!!!!. 💘',
+  timer: 2000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading()
+    const b = Swal.getHtmlContainer().querySelector('b')
+    timerInterval = setInterval(() => {
+      b.textContent = Swal.getTimerLeft()
+    }, 100)
+  },
+  willClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
+
+});
+
